bc6df791df1cc7580960e0175e85a68ede43cbba
Jonas Bakken <jonasbakken@protonmail.com>
2022-04-26 21:33:09 +0200

    Render all pages in pdf

--- src/render/pdf.rs
+++ src/render/pdf.rs
@@ -10,35 +10,34 @@ pub fn render_pdf(path: &str, pages: &[Page]) -> io::Result<()> {
     // Open our pdf document.
     let mut document = Pdf::create(path).expect("Create PDF file");
 
-    // Only one page to consider.
-    let page = &pages[0];
+    for page in pages {
+        document.render_page(1404.0, 1872.0, |c| {
+            // Inverse Y coordinate system.
+            c.concat(Matrix::scale(1., -1.))?;
+            c.concat(Matrix::translate(0., -1872.))?;
 
-    document.render_page(1404.0, 1872.0, |c| {
-        // Inverse Y coordinate system.
-        c.concat(Matrix::scale(1., -1.))?;
-        c.concat(Matrix::translate(0., -1872.))?;
+            c.set_stroke_color(graphicsstate::Color::gray(0))?;
 
-        c.set_stroke_color(graphicsstate::Color::gray(0))?;
-
-        for layer in &page.layers {
-            for line in &layer.lines {
-                if line.points.is_empty() {
-                    continue;
+            for layer in &page.layers {
+                for line in &layer.lines {
+                    if line.points.is_empty() {
+                        continue;
+                    }
+                    let first_point = &line.points[0];
+                    c.move_to(first_point.x, first_point.y)?;
+                    for point in &line.points {
+                        c.set_line_width(point.pressure * BASE_LINE_WIDTH)?;
+                        c.set_line_cap_style(CapStyle::Round)?;
+                        c.set_line_join_style(JoinStyle::Round)?;
+                        c.line_to(point.x, point.y)?;
+                    }
+                    c.stroke()?;
                 }
-                let first_point = &line.points[0];
-                c.move_to(first_point.x, first_point.y)?;
-                for point in &line.points {
-                    c.set_line_width(point.pressure * BASE_LINE_WIDTH)?;
-                    c.set_line_cap_style(CapStyle::Round)?;
-                    c.set_line_join_style(JoinStyle::Round)?;
-                    c.line_to(point.x, point.y)?;
-                }
-                c.stroke()?;
             }
-        }
 
-        Ok(())
-    })?;
+            Ok(())
+        })?;
+    }
     document.finish()?;
     Ok(())
 }
